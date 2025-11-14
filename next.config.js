7922858751bbd36bc502c8c7bb70d7a9acbd6bdc
Bompredio <riquelmesantana112@gmail.com>
2025-11-14 22:21:22 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-  
-  },
+  // Configuração básica - removemos experimental.appDir
 }
-
-module.exports = nextConfig
