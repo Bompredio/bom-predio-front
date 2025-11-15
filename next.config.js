34997ecd1f4643d5aa7e34e7977d669795b28e5b
Bompredio <riquelmesantana112@gmail.com>
2025-11-15 16:19:57 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    optimizePackageImports: ['@supabase/supabase-js']
-  }
+  // Configuração mais simples
 }
 
 module.exports = nextConfig
