ae5107174c9b4213e2cf7963c7f9611dbd2f9cd1
Bompredio <riquelmesantana112@gmail.com>
2025-11-15 16:51:57 +0000

    Update List.tsx

--- app/components/ratings/List.tsx
+++ app/components/ratings/List.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { supabase } from '@/lib/supabase';
-import RatingStars from './RatingStars';
+import RatingStars from './Stars';
 
 interface Rating {
   id: string;
