c04f175b8fa9aef75fd61cd7f0881218eb1614ca
Bompredio <riquelmesantana112@gmail.com>
2025-11-17 00:16:33 +0000

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: { appDir: true },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname),
+      '@components': path.resolve(__dirname, 'components'),
+      '@lib': path.resolve(__dirname, 'lib'),
+      '@pages': path.resolve(__dirname, 'pages'),
+      '@ui': path.resolve(__dirname, 'components/ui')
+    };
+    return config;
+  }
+};
+
+module.exports = nextConfig;
