0a9fcb0c863a6d1a1c13db3491c52a15323aafcf
Bompredio <riquelmesantana112@gmail.com>
2025-11-17 13:26:17 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-import App from '@/components/App'
+// components/App.tsx
+'use client'
 
-export default function Page() {
-  return <App />
+import React from 'react'
+import ErrorBoundary from './components/ErrorBoundary'
+import Home from './pages/Home'
+/* outros imports... */
+
+export default function App() {
+  return (
+    <ErrorBoundary>
+      <main>
+        <Home />
+        {/* resto do app */}
+      </main>
+    </ErrorBoundary>
+  )
 }
