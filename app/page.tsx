459715e1ae7e8edc1326808ea2c5bff639451337
Bompredio <riquelmesantana112@gmail.com>
2025-11-17 00:36:29 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import App from '../components/App.tsx'
+import X from '@/components/ui/sonner'
+import Tooltip from '@/components/ui/tooltip'
+import NotFound from '@/pages/NotFound'
+import { ThemeProvider } from './contexts/ThemeContext'
+import Home from './pages/Home'
 
 export default function Page(){
   return <div style={{padding:24}}><App/></div>
