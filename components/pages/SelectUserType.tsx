2254a2355cd29504ee22cd72a19c61105c95702b
Bompredio <riquelmesantana112@gmail.com>
2025-11-17 13:10:46 +0000

    Create SelectUserType.tsx

---
+++ components/pages/SelectUserType.tsx
@@ -0,0 +1,8 @@
+export default function SelectUserType() {
+  return (
+    <section style={{ padding: 24 }}>
+      <h1>Escolher tipo de utilizador (placeholder)</h1>
+      <p>Opções: Proprietário / Administrador / Prestador de serviços</p>
+    </section>
+  )
+}
