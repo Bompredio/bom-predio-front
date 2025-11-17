ed839b2778d3663186211fbbb1b7d71b7e27ee9b
Bompredio <riquelmesantana112@gmail.com>
2025-11-17 15:19:12 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Verificação segura do elemento root
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error('Elemento root não encontrado! Verifique o index.html')
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 )
