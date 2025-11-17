354bd8680267dc4ed7b4459e3e726b3a9b85bdfa
Bompredio <riquelmesantana112@gmail.com>
2025-11-17 13:52:43 +0000

    Delete app directory

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: 'Bom Prédio' }
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="pt-PT">
-      <body>{children}</body>
-    </html>
-  )
-}
