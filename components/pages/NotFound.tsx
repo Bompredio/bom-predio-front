b789ecec30a9a73f29f9d4a27a0828b6d9dc07c7
Bompredio <riquelmesantana112@gmail.com>
2025-11-17 14:16:37 +0000

    Delete components directory

--- components/pages/NotFound.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h2>Página não encontrada</h2>
-      <p>Voltamos já.</p>
-    </div>
-  )
-}
--- components/pages/SelectUserType.tsx
+++
@@ -1,8 +0,0 @@
-export default function SelectUserType() {
-  return (
-    <section style={{ padding: 24 }}>
-      <h1>Escolher tipo de utilizador (placeholder)</h1>
-      <p>Opções: Proprietário / Administrador / Prestador de serviços</p>
-    </section>
-  )
-}
