82ac93e8d7d7770878a843565ffbf494e402b534
Bompredio <riquelmesantana112@gmail.com>
2025-11-17 13:03:00 +0000

    Create Home.tsx

---
+++ components/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <section style={{ padding: 24 }}>
+      <h1>Home — Bom Prédio</h1>
+      <p>Conteúdo temporário enquanto migramos para Next.js</p>
+    </section>
+  )
+}
