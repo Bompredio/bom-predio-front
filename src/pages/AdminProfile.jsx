53023b8a2836c7b3c8635a13940c8753ca0ae568
Bompredio <riquelmesantana112@gmail.com>
2025-11-18 15:46:28 +0000

    Delete src directory

--- src/pages/AdminProfile.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import RatingStars from '../components/data/BreakdownBar'
-export default function AdminProfile(){
-  return <div className="container"><h2>Perfil Administradora</h2></div>
-}
--- src/supabase/01_create_leads.sql
+++
@@ -1,1 +0,0 @@
--- SQL leads placeholder
